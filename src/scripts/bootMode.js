a8ed7fa382813cee177b40180611980d2c403598
andytblt <andythiebault@icloud.com>
2025-08-08 20:52:47 +0200

    Update bootMode.js

--- src/scripts/bootMode.js
+++ src/scripts/bootMode.js
@@ -7,17 +7,17 @@ function loadEnvironment() {
     const envFile = process.env.NODE_ENV === 'development' ? '.env.development' : '.env';
     const envPath = path.resolve(process.cwd(), envFile);
 
-    console.log(`${color.green}[${getTimestamp()}]${color.reset} [PROCESS] Loading environment variables from: ${envPath}${color.reset}`);
+    console.log(`${color.green}[${getTimestamp()}]${color.reset} [PROCESS] Chargement des variables d'environnement depuis : ${envPath}${color.reset}`);
     if (process.env.NODE_ENV === 'development') { 
-        console.log(`${color.green}[${getTimestamp()}]${color.reset} [PROCESS] ${process.env.NODE_ENV} mode has been loaded!${color.reset}`);
+        console.log(`${color.green}[${getTimestamp()}]${color.reset} [PROCESS] ${process.env.NODE_ENV} le mode a été chargé !${color.reset}`);
     } else {
-        console.log(`${color.green}[${getTimestamp()}]${color.reset} [PROCESS] Non-development mode has been loaded!${color.reset}`);
+        console.log(`${color.green}[${getTimestamp()}]${color.reset} [PROCESS] Le mode non-développement a été chargé !${color.reset}`);
     }
 
     if (fs.existsSync(envPath)) {
         dotenv.config({ path: envPath });
     } else {
-        console.log(`${color.red}[${getTimestamp()}] [ERROR] Environment file ${envFile} not found${color.reset}`);
+        console.log(`${color.red}[${getTimestamp()}] [ERROR] Fichier d'environnement ${envFile} not found${color.reset}`);
         process.exit(1);
     }
 }
